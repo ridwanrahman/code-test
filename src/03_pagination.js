c8a600e35603be915032215b18b9bb29d8e3ef1a
ridwanrahman <ridwan_rahman@ymail.com>
2020-12-18 15:21:13 +1100

    Fix logic for question 3

---
+++ src/03_pagination.js
@@ -0,0 +1,48 @@
+/**
+ *  Returns an array based on the pageNumber and itemsPerPage from pageData
+ * @param {number} pageNumber
+ * @param {number} itemsPerPage
+ * @param {Array<string>} pageData
+ */
+function solution (pageNumber, itemsPerPage, pageData) {
+  if (pageNumber==0 || pageNumber==-1) {
+    pageNumber = 1;
+  }
+  var result = data.slice((pageNumber - 1) * itemsPerPage, pageNumber * itemsPerPage);
+  if (result == "") {
+    return null;
+  } else {
+    return result;
+  }
+}
+
+const data = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+]
+
+module.exports = { solution, data }
